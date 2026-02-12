074593702be88047529fcabbd990dd2dfd9ffdb1
LM-Life <baker8806@live.com>
2026-02-12 12:27:26 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this when you want to force all clients to update
-const CACHE_VERSION = 'v12.28';  // <-- change this to a new value on each major update
+const CACHE_VERSION = 'v12.29';  // <-- change this to a new value on each major update
 const CACHE_NAME = `c17-study-cache-${CACHE_VERSION}`;
 
 const ASSETS = [
